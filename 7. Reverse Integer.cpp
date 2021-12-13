e8d16f55f572e99ce45331138484ba920d53693a
Ankit Kumar <ankit.kumar.061000@gmail.com>
2021-12-13 23:24:23 +0530

    Time: 0 ms (100%), Space : 6 MB (32.18%)

---
+++ 7. Reverse Integer.cpp
@@ -0,0 +1,16 @@
+int reverse(int x)
+{
+    int rev=0;
+    while(x!=0)
+    {
+    if (rev > INT_MAX/10 || (rev == INT_MAX / 10 && x%10 > 7)) return 0;
+    if (rev < INT_MIN/10 || (rev == INT_MIN / 10 && x%10 < -8)) return 0;
+        rev = rev*10 + (x%10);
+        x/=10;
+    }
+    return rev;
+};
+© 2021 GitHub, Inc.
+Terms
+Privacy
+Security
