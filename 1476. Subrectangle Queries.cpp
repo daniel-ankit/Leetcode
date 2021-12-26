f0abaf4039a2feeb86835a4d9ec600a4c423cfde
Ankit Kumar <ankit.kumar.061000@gmail.com>
2021-12-26 12:33:04 +0530

    Time : 28 ms (99.56%), Space : 18.5MB (73.90%)

---
+++ 1476. Subrectangle Queries.cpp
@@ -0,0 +1,30 @@
+class SubrectangleQueries
+{
+public:
+    vector<vector<int>> V;
+    SubrectangleQueries(vector<vector<int>>& rectangle)
+    {
+        copy(rectangle.begin(), rectangle.end(), back_inserter(V));
+    }
+    
+    void updateSubrectangle(int row1, int col1, int row2, int col2, int newValue)
+    {
+        for(int i=row1; i<=row2; i++)
+        {
+            for(int j=col1; j<=col2; j++)
+                V[i][j]=newValue;
+        }
+    }
+    
+    int getValue(int row, int col)
+    {
+        return V[row][col];
+    }
+};
+
+/**
+ * Your SubrectangleQueries object will be instantiated and called as such:
+ * SubrectangleQueries* obj = new SubrectangleQueries(rectangle);
+ * obj->updateSubrectangle(row1,col1,row2,col2,newValue);
+ * int param_2 = obj->getValue(row,col);
+ */
