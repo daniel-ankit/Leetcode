4daf253ad11cf45264e438e121eb9636a4900245
Ankit Kumar <ankit.kumar.061000@gmail.com>
2021-12-28 17:21:53 +0530

    Time : 0 ms (100%), Space : 9.1MB (72.80%)

---
+++ 0088. Merge Sorted Array.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n)
+    {
+        for(int i=m, j=0; i<n+m, j<n; i++, j++)
+            nums1[i] = nums2[j];
+        sort(nums1.begin(), nums1.end());
+    }
+};
