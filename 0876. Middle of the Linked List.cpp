4361f913778fa4146988c40c0a5f9048d33a4165
Ankit Kumar <ankit.kumar.061000@gmail.com>
2021-12-28 13:46:09 +0530

    Time : 0 ms (100%), Space : 7.2MB (21.25%)

---
+++ 0876. Middle of the Linked List.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head)
+    {
+        if(head == NULL || head->next == NULL) return head;
+        ListNode *slow = head, *fast = head->next;
+        int length=0;
+        while(slow!=NULL)
+        {
+            slow = slow->next;
+            length++;
+        }
+        slow = head;
+        while(fast!=NULL && fast->next!=NULL)
+        {
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        if(length%2) return slow;
+        return slow->next;
+    }
+};
