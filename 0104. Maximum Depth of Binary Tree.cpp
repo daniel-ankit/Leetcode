1757f4c16a6a7aed5a371600d122a10467ec1edd
Ankit Kumar <ankit.kumar.061000@gmail.com>
2022-01-23 23:01:33 +0530

    Time: 4 ms (94.33%), Space : 18.9MB (55.59%)

---
+++ 0104. Maximum Depth of Binary Tree.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root)
+    {
+        if(root==NULL) return 0;
+        return 1+ max(maxDepth(root->left), maxDepth(root->right));
+    }
+};
+
+int main()
+{
+    
+    return 69;
+}
