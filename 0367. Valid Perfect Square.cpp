415077669e088ba64528f176d05974d4968227ce
Ankit Kumar <ankit.kumar.061000@gmail.com>
2022-01-24 00:47:28 +0530

    Time : 0 ms (100%), Space : 5.8MB (85.93%)

---
+++ 0367. Valid Perfect Square.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        int n=sqrt(num);
+        if(n*n==num) return 1;
+        return 0;
+    }
+};
+
+int main()
+{
+    Solution object;
+    cout << object.isPerfectSquare(69);
+    return 69;
+}
