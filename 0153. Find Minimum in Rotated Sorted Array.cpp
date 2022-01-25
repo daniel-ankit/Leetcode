f0f07599a49e81a3408dbe5b9264e3bc0684dbbc
Ankit Kumar <ankit.kumar.061000@gmail.com>
2022-01-25 21:40:51 +0530

    Time : 2 ms (71.93%), Space : 10.2MB (71.27%)

---
+++ 0153. Find Minimum in Rotated Sorted Array.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int findMin(vector<int>& nums)
+    {
+        int ans = nums[0];
+        for(int i=1; i<nums.size(); i++)
+        {
+            if(nums[i]<ans)
+                return nums[i];
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+    
+    return 69;
+}
