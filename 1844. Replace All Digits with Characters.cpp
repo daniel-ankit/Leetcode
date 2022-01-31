9dce8baffc0bec8d96aeac6b4091631ba1ddeb37
Ankit Kumar <ankit.kumar.061000@gmail.com>
2022-01-31 14:14:46 +0530

    Time : 0 ms (100%), Space : 6MB (82.27%)

---
+++ 1844. Replace All Digits with Characters.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    string replaceDigits(string s)
+    {
+        for(int i=0; i<s.length()-1; i+=2)
+            s[i+1] = s[i]+s[i+1]-'0';
+        return s;
+    }
+    
+};
+
+int main()
+{
+    Solution object;
+    return 69;
+}
