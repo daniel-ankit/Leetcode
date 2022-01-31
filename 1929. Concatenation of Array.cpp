d6c35e4fdb9b011dbd038e8e716299681f71870b
Ankit Kumar <ankit.kumar.061000@gmail.com>
2022-01-31 13:39:36 +0530

    Time : 8 ms (84.77%), Space : 12.6MB (62.05%)

---
+++ 1929. Concatenation of Array.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums)
+    {
+        int n = nums.size();
+        vector<int> ans = nums;
+        for(int i= 0; i<n; i++)
+            ans.push_back(nums[i]);
+        return ans;
+        
+    }
+};
+
+int main()
+{
+    
+    return 69;
+}
