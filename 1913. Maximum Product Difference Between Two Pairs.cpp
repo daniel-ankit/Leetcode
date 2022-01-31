5bf9ff1f269b879c461edf452704884a0a6bd7ac
Ankit Kumar <ankit.kumar.061000@gmail.com>
2022-01-31 13:44:28 +0530

    Time : 24 ms (92.19%), Space : 20.4MB (63.97%)

---
+++ 1913. Maximum Product Difference Between Two Pairs.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int maxProductDifference(vector<int>& nums)
+    {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        int N = nums.size();
+        sort(nums.begin(), nums.end());
+        return (nums[N-1]*nums[N-2]-nums[1]*nums[0]);
+    }
+};
+
+int main()
+{
+    Solution object;
+    return 69;
+}
