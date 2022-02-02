1af08e7ab31abb44e40e827d0400421e066950d0
Ankit Kumar <ankit.kumar.061000@gmail.com>
2022-02-03 00:56:13 +0530

    Time : 0 ms (100%), Space : 5.7MB (95.39%)

---
+++ 1227. Airplane Seat Assignment Probability.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    double nthPersonGetsNthSeat(int n) {
+        if(n==1) return double(1);
+        return double(1)/2; // either he gets his seat or not
+    }
+};
+
+int main()
+{
+    Solution object;
+    
+    return 69;
+}
