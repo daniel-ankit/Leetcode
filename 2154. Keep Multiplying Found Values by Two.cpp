86f80436cdde8af48bc4f7ca53e9cd6cff65ba71
Ankit Kumar <ankit.kumar.061000@gmail.com>
2022-02-03 02:23:00 +0530

    Time : 4 ms (96.24%), Space : 10.5MB (91.26%)

---
+++ 2154. Keep Multiplying Found Values by Two.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int findFinalValue(vector<int>& nums, int original)
+    {
+        while(find(nums.begin(), nums.end(), original)!=nums.end())
+            original*=2;
+        return original;
+    }
+};
+
+int main()
+{
+    Solution object;
+    
+    return 69;
+}
