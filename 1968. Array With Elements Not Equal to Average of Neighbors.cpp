0d4224673243012fe743df0404c156a944405404
Ankit Kumar <ankit.kumar.061000@gmail.com>
2022-02-12 14:31:09 +0530

    Time : 224 ms (86.03%), Space : 126.8MB (28.93%)

---
+++ 1968. Array With Elements Not Equal to Average of Neighbors.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums)
+    {
+        vector<int> temp = nums;
+        sort(temp.begin(), temp.end());
+        int j = nums.size()-1;
+        for(int i=1; i<nums.size(); i+=2)
+            nums[i] = temp[j--];
+        j=0;
+        for(int i=0; i<nums.size(); i+=2)
+            nums[i] = temp[j++];
+        return nums;
+    }
+};
+
+
+int main()
+{
+    Solution object;
+    
+    return 69;
+}
