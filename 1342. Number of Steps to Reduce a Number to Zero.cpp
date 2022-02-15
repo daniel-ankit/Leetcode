e3f8b3418d6b13222220be1785e597930e74b051
Ankit Kumar <ankit.kumar.061000@gmail.com>
2022-02-16 02:32:58 +0530

    Time : 0 ms (100%), Space : 5.8MB (75.50%)

---
+++ 1342. Number of Steps to Reduce a Number to Zero.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int numberOfSteps(int num)
+    {
+        int ans=0;
+        while(num)
+        {
+            (num%2) ? (num--) : (num/=2);
+            ans++;
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+    Solution object;
+    
+    return 69;
+}
