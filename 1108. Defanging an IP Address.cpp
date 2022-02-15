4a85ff59cd0b3821eee78d8b218e14ecad39e8f5
Ankit Kumar <ankit.kumar.061000@gmail.com>
2022-02-16 01:49:29 +0530

    Time : 0 ms (100%), Space : 6MB (46.74%)

---
+++ 1108. Defanging an IP Address.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    string defangIPaddr(string address)
+    {
+        string S="";
+        for(int i=0; i<address.length(); i++)
+        {
+            if(address[i]=='.')
+                S+="[.]";
+            else S+=address[i];
+        }
+        return S;
+    }
+};
+
+int main()
+{
+    Solution object;
+    
+    return 69;
+}
