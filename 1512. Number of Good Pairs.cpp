304ca34e8a88bc4397c4aa7288987e9e54a496c1
Ankit Kumar <ankit.kumar.061000@gmail.com>
2022-02-16 02:19:54 +0530

    Time : 0 ms(100%), Space : 7.3MB (15.51%)

---
+++ 1512. Number of Good Pairs.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums)
+    {
+        int count=0;
+        sort(nums.begin(), nums.end());
+        for(int i=0; i<nums.size(); i++)
+        {
+            for(int j=i+1; j<nums.size(); j++)
+            {
+                if(nums[i]!=nums[j]) break;
+                count++;
+            }
+        }
+        return count;
+    }
+};
+
+int main()
+{
+    Solution object;
+    
+    return 69;
+}
