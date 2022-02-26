5d25a9f946d51ff5c37e4eea0110ec6a34e128c2
Ankit Kumar <ankit.kumar.061000@gmail.com>
2022-02-27 01:44:20 +0530

    Time : 12 ms (87.08%), Space : 7.5MB (73.11%)

---
+++ 0237. Delete Node in a Linked List.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        if(node==NULL || node->next==NULL)
+            node = NULL;
+        else
+        {
+            node->val = node->next->val;
+            node->next = node->next->next;
+        }
+    }
+};
+
+int main()
+{
+    Solution object;
+    
+    return 69;
+}
