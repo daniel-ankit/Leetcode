be7f08c046b90a20089e3c76b468436f9c163579
Ankit Kumar <ankit.kumar.061000@gmail.com>
2022-02-27 19:53:05 +0530

    Time : 28 ms (29.74%), Space : 10.6MB (88.16%)

---
+++ 0559. Maximum Depth of N-ary Tree.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+class Solution {
+public:
+    int maxDepth(Node* root) {
+        if(root==NULL) return 0;
+        int maxx = 0;
+        for(int i=0; i<root->children.size(); i++)
+            maxx = max(maxx, maxDepth(root->children[i]));
+        return 1+maxx;
+    }
+};
+
+int main()
+{
+    Solution object;
+    
+    return 69;
+}
