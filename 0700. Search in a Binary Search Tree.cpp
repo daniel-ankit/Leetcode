788477c81c0d2b6fc78441011ab9bd6d609e6920
Ankit Kumar <ankit.kumar.061000@gmail.com>
2022-03-07 20:37:00 +0530

    Time : 0 ms (100%), Space : 30MB (100%)

---
+++ 0700. Search in a Binary Search Tree.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        if(root==NULL || root->val == val) return root;
+        if(root->val < val) return searchBST(root->right, val);
+        return searchBST(root->left, val);
+    }
+};
+
+int main()
+{
+    Solution object;
+    
+    return 69;
+}
