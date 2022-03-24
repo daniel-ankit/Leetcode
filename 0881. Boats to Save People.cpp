0cdf7ffd148c57ef9cef386fdcfbe0dc87cbf9bb
Ankit Kumar <ankit.kumar.061000@gmail.com>
2022-03-24 14:36:09 +0530

    Time : 106 ms (55.11%), Space : 42MB (57.42%)

---
+++ 0881. Boats to Save People.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    
+};
+
+int main()
+{
+    Solution object;
+    
+    return 69;
+}
