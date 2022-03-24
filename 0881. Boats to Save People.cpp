670f07d0f671430ff8e5c5c44fdaf33786bae661
Ankit Kumar <88437909+daniel-ankit@users.noreply.github.com>
2022-03-24 14:38:22 +0530

    Time : 106 ms (55.11%), Space : 42MB (57.42%)

--- 0881. Boats to Save People.cpp
+++ 0881. Boats to Save People.cpp
@@ -21,7 +21,17 @@ struct TreeNode {
 
 class Solution {
 public:
-    
+    int numRescueBoats(vector<int>& people, int limit)
+    {
+        sort(people.begin(), people.end());
+        int i = 0, j = people.size()-1, ans=0;
+        while(i<=j)
+        {
+            if(people[i] + people[j] <= limit) i++;
+            ans++;
+            j--;
+        return ans;
+    }
 };
 
 int main()
