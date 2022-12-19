cf2051c9b5c81861d1b93f6fada75469f8afcf2f
Ankit Kumar <88437909+daniel-ankit@users.noreply.github.com>
2022-12-20 00:14:35 +0530

    Added solution - LeetHub

---
+++ Index of the First Occurrence of pattern in a text - GFG/index-of-the-first-occurrence-of-pattern-in-a-text.cpp
@@ -0,0 +1,32 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int findMatching(string text, string pat) {
+        if(text.size()<pat.size()) return -1;
+        for(int i=0; i<text.size()-pat.size()+1; i++)
+        {
+            if(text.substr(i, pat.size())==pat) return i;
+        }
+        return -1;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string text, pat;
+        cin >> text >> pat;
+        Solution obj;
+        cout << obj.findMatching(text, pat) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
