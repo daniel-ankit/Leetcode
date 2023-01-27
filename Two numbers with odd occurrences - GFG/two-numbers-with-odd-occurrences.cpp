f96c954710799b0e45ce1ea8b9b19ab5f66172e0
Ankit Kumar <88437909+daniel-ankit@users.noreply.github.com>
2023-01-27 23:54:21 +0530

    Added solution - LeetHub

---
+++ Two numbers with odd occurrences - GFG/two-numbers-with-odd-occurrences.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution{
+    public:
+    vector<long long int> twoOddNum(long long int Arr[], long long int N)  
+    {
+        vector<long long int> ans;
+        sort(Arr, Arr+N);
+        int i=0;
+        while(i<N)
+        {
+            if(Arr[i]==Arr[i+1]) i+=2;
+            else ans.push_back(Arr[i++]);
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        long long int N;
+        cin>>N;
+        long long int Arr[N];
+        for(long int i = 0;i < N;i++)
+        cin>>Arr[i];
+        Solution ob;
+        vector<long long int>ans=ob.twoOddNum(Arr,N);
+        for(long long int i=0;i<ans.size();i++)cout<<ans[i]<<" ";
+        cout<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
