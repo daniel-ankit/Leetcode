c9cae7b30ce56e67b63c59da07303ba4d8ec357b
Ankit Kumar <88437909+daniel-ankit@users.noreply.github.com>
2023-01-31 21:18:26 +0530

    Time: 20 ms (47.41%), Space: 9.1 MB (22.76%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -8,7 +8,21 @@ class Solution {
     }
 public:
     int minDistance(string s1, string s2) {
-        vector<vector<int>>dp(s1.size(), vector<int>(s2.size(), -1));
-        return fn(s1.size()-1, s2.size()-1, s1, s2, dp);
+        // vector<vector<int>>dp(s1.size()+1, vector<int>(s2.size()+1, -1));
+        // return fn(s1.size()-1, s2.size()-1, s1, s2, dp);
+        
+        vector<vector<int>>dp(s1.size()+1, vector<int>(s2.size()+1, 0));
+        for(int i=0; i<=s1.size(); i++) dp[i][0] = i;
+        for(int i=0; i<=s2.size(); i++) dp[0][i] = i;
+        
+        for(int i=1; i<=s1.size(); i++)
+        {
+            for(int j=1; j<=s2.size(); j++)
+            {
+                if(s1[i-1]==s2[j-1]) dp[i][j] = dp[i-1][j-1];
+                else dp[i][j] = min({dp[i-1][j], dp[i-1][j-1], dp[i][j-1]})+1;
+            }
+        }
+        return dp[s1.size()][s2.size()];
     }
 };
