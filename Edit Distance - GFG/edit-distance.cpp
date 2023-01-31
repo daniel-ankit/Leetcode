937e9082b8a732cb1b54669892ea85144dddb67f
Ankit Kumar <88437909+daniel-ankit@users.noreply.github.com>
2023-01-31 21:33:50 +0530

    Added solution - LeetHub

--- Edit Distance - GFG/edit-distance.cpp
+++ Edit Distance - GFG/edit-distance.cpp
@@ -6,18 +6,20 @@ using namespace std;
 class Solution {
   public:
     int editDistance(string s, string t) {
-        int n = s.size(), m = t.size(), dp[n+1][m+1] = {0};
-        for(int i=0; i<=n; i++) dp[i][0] = i;
+        int n = s.size(), m = t.size();
+        vector<vector<int>>dp(2, vector<int>(m+1, 0));
         for(int j=0; j<=m; j++) dp[0][j] = j;
         for(int i=1; i<=n; i++)
         {
+            dp[1][0] = i;
             for(int j=1; j<=m; j++)
             {
-                if(s[i-1]==t[j-1]) dp[i][j] = dp[i-1][j-1];
-                else dp[i][j] = min({dp[i-1][j], dp[i][j-1], dp[i-1][j-1]})+1;
+                if(s[i-1]==t[j-1]) dp[1][j] = dp[0][j-1];
+                else dp[1][j] = min({dp[0][j], dp[1][j-1], dp[0][j-1]})+1;
             }
+            dp[0] = dp[1];
         }
-        return dp[n][m];
+        return dp[1][m];
     }
 };
 
