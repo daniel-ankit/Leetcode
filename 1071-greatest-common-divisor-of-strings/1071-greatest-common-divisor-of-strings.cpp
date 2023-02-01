c0d404ba6bb89fb57f5bc6a68417276893c6183f
Ankit Kumar <88437909+daniel-ankit@users.noreply.github.com>
2023-02-01 21:46:03 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (65.71%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        if(str1+str2 != str2+str1) return "";
+        return str1.substr(0, __gcd(str1.size(), str2.size()));
+    }
+};
