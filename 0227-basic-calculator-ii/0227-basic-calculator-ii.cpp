e46374fbc534338d8a501438e7b57f24ffbe8077
Ankit Kumar <88437909+daniel-ankit@users.noreply.github.com>
2023-03-07 23:12:20 +0530

    Time: 23 ms (68.45%), Space: 11.4 MB (35.96%) - LeetHub

---
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int calculate(string s) {
+    istringstream in('+' + s + '+');
+    long long total = 0, term = 0, n;
+    char op;
+    while (in >> op) {
+        if (op == '+' or op == '-') {
+            total += term;
+            in >> term;
+            term *= 44 - op;
+        } else {
+            in >> n;
+            if (op == '*')
+                term *= n;
+            else
+                term /= n;
+        }
+    }
+    return total;
+}
+};
