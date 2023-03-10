8d3c12167155f474e8cde0290a01beae1d17206c
Ankit Kumar <88437909+daniel-ankit@users.noreply.github.com>
2023-03-10 17:01:44 +0530

    Time: 59 ms (82.04%), Space: 21.9 MB (70.82%) - LeetHub

---
+++ 1447-simplified-fractions/1447-simplified-fractions.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<string> simplifiedFractions(int n) {
+        vector<string>ans;
+        for(int i=1; i<n; i++)
+        {
+            for(int j=i+1; j<=n; j++)
+            {
+                if(__gcd(i,j)==1)
+                    ans.push_back(to_string(i) + '/' + to_string(j));
+            }
+        }
+        return ans;
+    }
+};
