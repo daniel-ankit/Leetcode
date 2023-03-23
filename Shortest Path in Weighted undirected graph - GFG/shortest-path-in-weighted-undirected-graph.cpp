6d583c33bd304c872b9ad6e9e46f1957c425f2ff
Ankit Kumar <88437909+daniel-ankit@users.noreply.github.com>
2023-03-24 01:07:13 +0530

    Added solution - LeetHub

--- Shortest Path in Weighted undirected graph - GFG/shortest-path-in-weighted-undirected-graph.cpp
+++ Shortest Path in Weighted undirected graph - GFG/shortest-path-in-weighted-undirected-graph.cpp
@@ -3,18 +3,19 @@
 using namespace std;
 
 // } Driver Code Ends
+
 class Solution {
   public:
     vector<int> shortestPath(int n, int m, vector<vector<int>>& edges) {
-        vector<vector<int>>adj[n+1];
+        vector<pair<int, int>>adj[n+1];
         for(auto x:edges)
         {
             adj[x[0]].push_back({x[1], x[2]});
             adj[x[1]].push_back({x[0], x[2]});
         }
-        vector<int>dist(n+1, INT_MAX), parent(n+1, 0);
-        for(int i=1; i<=n; i++) parent[i] = i;
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        vector<int>dist(n+1, INT_MAX), parent(n+1);
+        for(int i=0; i<=n; i++) parent[i] = i;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>>pq;
         pq.push({0,1});
         dist[1] = 0;
         while(!pq.empty())
@@ -23,24 +24,61 @@ class Solution {
             pq.pop();
             for(auto x:adj[node])
             {
-                if(dis + x[1] < dist[x[0]])
+                if(dist[x.first] > dis + x.second)
                 {
-                    parent[x[0]] = node;
-                    dist[x[0]] = dis+x[1];
-                    pq.push({dist[x[0]], x[0]});
+                    parent[x.first] = node;
+                    dist[x.first] = dis + x.second;
+                    pq.push({dist[x.first], x.first});
                 }
             }
         }
         if(dist[n]==INT_MAX) return {-1};
-        vector<int> path(1, n);
-        int i=n;
+        vector<int>ans;
+        int i = n;
         while(i!=1)
         {
-            path.push_back(parent[i]);
+            ans.push_back(i);
             i = parent[i];
         }
-        reverse(path.begin(), path.end());
-        return path;
+        ans.push_back(1);
+        reverse(ans.begin(), ans.end());
+        return ans;
+        
+        // vector<vector<int>>adj[n+1];
+        // for(auto x:edges)
+        // {
+        //     adj[x[0]].push_back({x[1], x[2]});
+        //     adj[x[1]].push_back({x[0], x[2]});
+        // }
+        // vector<int>dist(n+1, INT_MAX), parent(n+1, 0);
+        // for(int i=1; i<=n; i++) parent[i] = i;
+        // priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        // pq.push({0,1});
+        // dist[1] = 0;
+        // while(!pq.empty())
+        // {
+        //     auto node = pq.top().second, dis = pq.top().first;
+        //     pq.pop();
+        //     for(auto x:adj[node])
+        //     {
+        //         if(dis + x[1] < dist[x[0]])
+        //         {
+        //             parent[x[0]] = node;
+        //             dist[x[0]] = dis+x[1];
+        //             pq.push({dist[x[0]], x[0]});
+        //         }
+        //     }
+        // }
+        // if(dist[n]==INT_MAX) return {-1};
+        // vector<int> path(1, n);
+        // int i=n;
+        // while(i!=1)
+        // {
+        //     path.push_back(parent[i]);
+        //     i = parent[i];
+        // }
+        // reverse(path.begin(), path.end());
+        // return path;
     }
 };
 
