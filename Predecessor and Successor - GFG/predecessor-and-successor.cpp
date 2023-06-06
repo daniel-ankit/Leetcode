5b1135dd43cafbefa5c82aeb3c2c632ed5b41fc5
Ankit Kumar <88437909+daniel-ankit@users.noreply.github.com>
2023-06-06 16:23:38 +0530

    Added solution - LeetHub

---
+++ Predecessor and Successor - GFG/predecessor-and-successor.cpp
@@ -0,0 +1,155 @@
+//{ Driver Code Starts
+// C++ program to find predecessor and successor in a BST
+#include "bits/stdc++.h"
+using namespace std;
+
+// BST Node
+struct Node
+{
+	int key;
+	struct Node *left;
+	struct Node *right;
+	
+	Node(int x){
+	    key = x;
+	    left = NULL;
+	    right = NULL;
+	}
+};
+
+
+// } Driver Code Ends
+/* BST Node
+struct Node
+{
+	int key;
+	struct Node *left;
+	struct Node *right;
+	
+	Node(int x){
+	    key = x;
+	    left = NULL;
+	    right = NULL;
+	}
+};
+*/
+
+class Solution
+{
+    public:
+    void findPreSuc(Node* root, Node*& pre, Node*& suc, int key)
+    {
+        Node* temp = root;
+        while(temp)
+        {
+            if(temp->key >= key) temp = temp->left;
+            else
+            {
+                pre = temp;
+                temp = temp->right;
+            }
+        }
+        temp = root;
+        while(temp)
+        {
+            if(temp->key > key)
+            {
+                suc = temp;
+                temp = temp->left;
+            }
+            else temp = temp->right;
+        }
+    }
+};
+
+
+//{ Driver Code Starts.
+
+Node* buildTree(string str)
+{
+   // Corner Case
+   if(str.length() == 0 || str[0] == 'N')
+       return NULL;
+
+   // Creating vector of strings from input
+   // string after spliting by space
+   vector<string> ip;
+
+   istringstream iss(str);
+   for(string str; iss >> str; )
+       ip.push_back(str);
+
+   // Create the root of the tree
+   Node* root = new Node(stoi(ip[0]));
+
+   // Push the root to the queue
+   queue<Node*> queue;
+   queue.push(root);
+
+   // Starting from the second element
+   int i = 1;
+   while(!queue.empty() && i < ip.size()) {
+
+       // Get and remove the front of the queue
+       Node* currNode = queue.front();
+       queue.pop();
+
+       // Get the current node's value from the string
+       string currVal = ip[i];
+
+       // If the left child is not null
+       if(currVal != "N") {
+
+           // Create the left child for the current node
+           currNode->left = new Node(stoi(currVal));
+
+           // Push it to the queue
+           queue.push(currNode->left);
+       }
+
+       // For the right child
+       i++;
+       if(i >= ip.size())
+           break;
+       currVal = ip[i];
+
+       // If the right child is not null
+       if(currVal != "N") {
+
+           // Create the right child for the current node
+           currNode->right = new Node(stoi(currVal));
+
+           // Push it to the queue
+           queue.push(currNode->right);
+       }
+       i++;
+   }
+
+   return root;
+}
+// Driver program to test above functions
+int main() {
+   
+   int t;
+   string tc;
+   getline(cin, tc);
+   t=stoi(tc);
+   while(t--)
+   {
+        string s; 
+        getline(cin, s);
+        Node* root = buildTree(s);
+        getline(cin, s);
+        int k = stoi(s);
+        Node *pre=NULL,*succ=NULL;
+        Solution ob;
+        ob.findPreSuc(root,pre,succ,k);
+        (pre!=NULL)?cout<<pre->key:cout<<-1;
+        cout<<" ";
+        (succ!=NULL)?cout<<succ->key:cout<<-1;
+        cout<<endl;
+        // inOrderTraversal(root);
+   }
+   return 0;
+}
+// } Driver Code Ends
