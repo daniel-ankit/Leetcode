8b9bb7c6351f2632f8726c7c30cbd253a647c4ac
Ankit Kumar <88437909+daniel-ankit@users.noreply.github.com>
2023-06-08 14:30:12 +0530

    Added solution - LeetHub

---
+++ Find Kth permutation - GFG/find-kth-permutation.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+public:
+    string kthPermutation(int n, int k)
+    {
+        string nums;
+        for(int i=1; i<=n; i++)
+            nums.push_back(i+'0');
+        while(--k)
+            next_permutation(nums.begin(), nums.end());
+        return nums;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, k;
+        cin >> n >> k;
+        Solution ob;
+        string ans = ob.kthPermutation(n, k);
+        cout << ans;
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
