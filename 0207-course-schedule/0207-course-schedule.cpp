06040543d062949c1db7ac1dad32194b3603a98c
Ankit Kumar <88437909+daniel-ankit@users.noreply.github.com>
2023-07-11 12:35:14 +0530

    Time: 22 ms (77.16%), Space: 13.7 MB (60.39%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
     bool canFinish(int n, vector<vector<int>>& pre) {
-        vector<int> adj[n], out(n, 0);
+        vector<int>indegree(n, 0);
+        int cnt = 0;
+        vector<int>adj[n];
         for(auto x:pre)
         {
             adj[x[1]].push_back(x[0]);
-            out[x[0]]++;
+            indegree[x[0]]++;
         }
-        queue<int> q;
+        queue<int>q;
         for(int i=0; i<n; i++)
         {
-            if(out[i]==0) q.push(i);
+            if(!indegree[i]) q.push(i);
         }
-        int count = 0;
         while(!q.empty())
         {
             auto now = q.front();
-            count++;
             q.pop();
+            cnt++;
             for(auto x:adj[now])
             {
-                out[x]--;
-                if(out[x]==0) q.push(x);
+                indegree[x]--;
+                if(!indegree[x]) q.push(x);
             }
         }
-        return count==n;
+        return cnt == n;
     }
 };
